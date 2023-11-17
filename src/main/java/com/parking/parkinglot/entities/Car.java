e3ffb8e72fb41df57ca1aef3aaea028c5a974722
EdiTira <eduard.tira@ulbsibiu.ro>
2023-11-17 10:44:46 +0200

    create the entities

---
+++ src/main/java/com/parking/parkinglot/entities/Car.java
@@ -0,0 +1,54 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Basic
+    private String licensePlate;
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    @Basic
+    private String parkingSpot;
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    private User owner;
+
+    @ManyToOne
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
